6cc86d38aea0622c19598c2be3958e4affc7b4d2
Irene <irene.sanchez.cano@gmail.com>
2024-05-28 15:22:50 +0200

    L

--- TETRIS/tetris.js
+++ TETRIS/tetris.js
@@ -30,7 +30,7 @@ function dibujarCuadrado() {
 }
 
 
-function dibujarrectangulo() {
+function dibujarRectangulo() {
     ctx.fillStyle = "yellow";
     ctx.fillRect(0, 0, 25, 100);
 
@@ -59,7 +59,91 @@ function dibujarrectangulo() {
     ctx.stroke();
 }
 
-dibujarCuadrado();
-//dibujarrectangulo()
+
+function dibujarL() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 25, 100);
+    ctx.fillRect(25, 75, 25, 25);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 1);
+    ctx.lineTo(25, 50);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 25);
+    ctx.lineTo(25, 25);
+    ctx.moveTo(0, 50);
+    ctx.lineTo(25, 50);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(25, 75);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(25, 0);
+    ctx.lineTo(25, 100);
+    ctx.lineTo(0, 100);
+    ctx.lineTo(0,0);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(25, 75);
+    ctx.lineTo(50, 75);
+    ctx.lineTo(50, 100);
+    ctx.lineTo(25,100);
+    ctx.stroke();
+}
+
+
+function dibujarLInvertida() {
+    ctx.fillStyle = "brown";
+    ctx.fillRect(25, 0, 25, 100);
+    ctx.fillRect(0, 75, 25, 25);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 0);
+    ctx.lineTo(25, 75);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(50, 0);
+    ctx.lineTo(50, 100);
+    ctx.moveTo(50, 100);
+    ctx.lineTo(0, 100);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(0, 100);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(25, 75);
+    ctx.lineTo(25, 100);
+    ctx.moveTo(25, 75);
+    ctx.lineTo(50, 75);
+    ctx.moveTo(25, 50);
+    ctx.lineTo(50, 50);
+    ctx.moveTo(25, 25);
+    ctx.lineTo(50, 25);
+    ctx.moveTo(25, 0);
+    ctx.lineTo(50, 0);
+    ctx.stroke();
+  }
+
+
+function dibujarZ() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 25, 100);
+    ctx.fillRect(0, 0, 25, 100);
+}
+
+//dibujarLInvertida();
+//dibujarL();
+//dibujarCuadrado();
+//dibujarRectangulo();
+
+
+
 
 //--------------------------------------------
