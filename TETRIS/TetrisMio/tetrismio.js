99bae52f36d6e30924837a4a25250f39fa2da921
irene <irene.sanchez.cano@gmail.com>
2024-05-30 15:30:38 +0200

    varios

---
+++ TETRIS/TetrisMio/tetrismio.js
@@ -0,0 +1,186 @@
+
+import './tetris.css'
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+/* 
+function dibujarCuadrado() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 50, 50);
+    
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 1);
+    ctx.lineTo(25, 50);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 25);
+    ctx.lineTo(50, 25);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 50);
+    ctx.lineTo(50, 50);
+    ctx.lineTo(50, 0);
+    ctx.lineTo(0, 0);
+    ctx.lineTo(0, 50);
+    ctx.stroke();
+}
+
+function dibujarRectangulo() {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 0, 25, 100);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 1);
+    ctx.lineTo(25, 50);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 25);
+    ctx.lineTo(25, 25);
+    ctx.moveTo(0, 50);
+    ctx.lineTo(25, 50);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(25, 75);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(25, 0);
+    ctx.lineTo(25, 100);
+    ctx.lineTo(0, 100);
+    ctx.lineTo(0,0);
+    ctx.stroke();
+}
+function dibujarL() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 25, 100);
+    ctx.fillRect(25, 75, 25, 25);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 1);
+    ctx.lineTo(25, 50);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 25);
+    ctx.lineTo(25, 25);
+    ctx.moveTo(0, 50);
+    ctx.lineTo(25, 50);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(25, 75);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(25, 0);
+    ctx.lineTo(25, 100);
+    ctx.lineTo(0, 100);
+    ctx.lineTo(0,0);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(25, 75);
+    ctx.lineTo(50, 75);
+    ctx.lineTo(50, 100);
+    ctx.lineTo(25,100);
+    ctx.stroke();
+}
+function dibujarLInvertida() {
+    ctx.fillStyle = "brown";
+    ctx.fillRect(25, 0, 25, 100);
+    ctx.fillRect(0, 75, 25, 25);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 0);
+    ctx.lineTo(25, 75);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(50, 0);
+    ctx.lineTo(50, 100);
+    ctx.moveTo(50, 100);
+    ctx.lineTo(0, 100);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(0, 100);
+    ctx.moveTo(0, 75);
+    ctx.lineTo(25, 75);
+    ctx.lineTo(25, 100);
+    ctx.moveTo(25, 75);
+    ctx.lineTo(50, 75);
+    ctx.moveTo(25, 50);
+    ctx.lineTo(50, 50);
+    ctx.moveTo(25, 25);
+    ctx.lineTo(50, 25);
+    ctx.moveTo(25, 0);
+    ctx.lineTo(50, 0);
+    ctx.stroke();
+  }
+function dibujarZ() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 50, 25);
+    ctx.fillRect(25, 25, 50, 25);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(50, 0);
+    ctx.lineTo(50, 25);
+    ctx.lineTo(75, 25);
+    ctx.lineTo(75, 50);
+    ctx.lineTo(25, 50);
+    ctx.lineTo(25, 25);
+    ctx.lineTo(0, 25);
+    ctx.lineTo(0, 0);
+    ctx.moveTo(25, 0);
+    ctx.lineTo(25, 25);
+    ctx.lineTo(50, 25);
+    ctx.lineTo(50, 50)
+    ctx.stroke();
+}
+function dibujarZInvertida(){
+    ctx.fillStyle = "purple";
+    ctx.fillRect(25, 0, 50, 25);
+    ctx.fillRect(0, 25, 50, 25);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(25, 0);
+    ctx.lineTo(75, 0);
+    ctx.lineTo(75, 25);
+    ctx.lineTo(50, 25);
+    ctx.lineTo(50, 50);
+    ctx.lineTo(0,50);
+    ctx.lineTo(0,25);
+    ctx.lineTo(25,25);
+    ctx.lineTo(25,0);
+    ctx.moveTo(25, 25);
+    ctx.lineTo(50,25);
+
+
+
+
+    ctx.stroke();
+
+
+}
+//dibujarZInvertida()
+//dibujarZ()
+//dibujarLInvertida();
+//dibujarL();
+//dibujarCuadrado();
+//dibujarRectangulo();
+
+ */
