e8b1e785e1865181f61ce85ff81bc46f93d3fd08
irene <irene.sanchez.cano@gmail.com>
2024-06-11 15:55:36 +0200

    hackaton

---
+++ Python/prueba.py
@@ -0,0 +1,27 @@
+
+# %%
+print(1+1)
+print('A')
+
+
+# %%
+def calcular_maximo(a: int, b: int)-> int:
+    if a > b:
+        return a
+    else:
+        return b
+    print (calcular_maximo)
+# %%
+
+
+#%% 2 - Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el mayor de ellos.
+def calcular_maximo_de_tres(a,b,c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+
+assert
+# %%
